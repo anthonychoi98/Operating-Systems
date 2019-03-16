64f92e78c6850aeb3c97d1404b4a14f8f7b01f6a
gmejia8 <46811523+gmejia8@users.noreply.github.com>
2019-03-16 12:54:41 -0700

    updated communicator

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -10,99 +10,79 @@ import nachos.machine.*;
  * threads can be paired off at this point.
  */
 public class Communicator {
-	Lock communicatorLock;
-	int message;
-	int listeners;
-	Condition2 listeningCondition;
-	Condition2 speakingCondition;
-	Integer buffer;
-	boolean messageReady;
-	
     /**
      * Allocate a new communicator.
      */
-	/*
-	 * Semaphore mutex = new Semaphore(1); Semaphore fullslots = new Semaphore(0);
-	 * Semaphore emptyslots = new Semaphore(1); Queue<Integer> words = new
-	 * LinkedList<>();
-	 */
-	/*
-	 * Semaphore mutex = new Semaphore(1); Semaphore fullslots = new Semaphore(0);
-	 * Semaphore emptyslots = new Semaphore(1); Queue<Integer> words = new
-	 * LinkedList<>();
-	 */
-	
     public Communicator() {
-    	communicatorLock = new Lock();
-    	message = 0;
-    	listeners = 0;
-    	listeningCondition = new Condition2(communicatorLock);
-    	speakingCondition = new Condition2(communicatorLock);
-    	buffer = null;
-    	messageReady = false;
+        lock = new Lock();
+        wordReady = false;
+        okToSpeak = new Condition2(lock);
+        okToListen = new Condition2(lock);
+        okToConnect = new Condition2(lock);
     }
 
     /**
-@ -35,11 +51,24 @@ public class Communicator {
+     * Wait for a thread to listen through this communicator, and then transfer
+     * <i>word</i> to the listener.
+     *
+     * <p>
+     * Does not return until this thread is paired up with a listening thread.
+     * Exactly one listener should receive <i>word</i>.
+     *
      * @param	word	the integer to transfer.
      */
     public void speak(int word) {
-		/*
-		 * emptyslots.P(); mutex.P(); words.add(word); mutex.V(); fullslots.V();
-		 */
-    	communicatorLock.acquire();
-    	
-    	while(listeners == 0 || buffer == null) {
-    		speakingCondition.sleep();
-    	}
-    	
-    	message = word;
-    	buffer = 1;
-    	
-    	listeningCondition.wake();
-    	
-    	
-    	communicatorLock.release();
-    	
-		/*
-		 * emptyslots.P(); mutex.P(); words.add(word); mutex.V(); fullslots.V();
-		 */
-		
+        lock.acquire();
+
+        // while a word is already ready
+        while(wordReady) {
+            okToSpeak.sleep();
+        }
+
+        // set the word to be spoken
+        this.word = word;
+
+        wordReady = true;
+
+        // wake sleeping listener
+        okToListen.wake();
+        okToConnect.sleep();
+
+        lock.release();
     }
 
     /**
-@ -49,11 +78,30 @@ public class Communicator {
+     * Wait for a thread to speak through this communicator, and then return
+     * the <i>word</i> that thread passed to <tt>speak()</tt>.
+     *
      * @return	the integer transferred.
-     */    
+     */
     public int listen() {
-		/*
-		 * fullslots.P(); mutex.P(); int word = words.remove(); emptyslots.V();
-		 * System.out.println("here"); return word;
-		 */
-    	communicatorLock.acquire();
-    	
-    	listeners++;
-    	
-    	while(buffer == null) {
-    		listeningCondition.sleep();
-    		speakingCondition.wake();
-    	}
-    	
-    	int msg = message;
-    	buffer = null;
-    	
-    	listeners--;
-    	
-    	communicatorLock.release();
-    	
-    	return msg;
-		
-		/*
-		 * fullslots.P(); mutex.P(); int word = words.remove(); emptyslots.V();
-		 * 
-		 * return word;
-		 */
-		 
+        lock.acquire();
+
+        // while a word is not ready to be heard
+        while(!wordReady) {
+            okToListen.sleep();
+        }
+
+        // heard word, so set wordReady back to false
+        wordReady = false;
+
+        // wake a sleeping speaker
+        okToSpeak.wake();
+        okToConnect.wake();
+
+        lock.release();
+
+	    return word;
     }
-    
+
+    private Lock lock;      // lock
+    private int word = 0;   // word to be spoken
+
+    private boolean wordReady;   // check if word is ready
+
+    private Condition2 okToSpeak;
+    private Condition2 okToListen;
+    private Condition2 okToConnect;
 }
