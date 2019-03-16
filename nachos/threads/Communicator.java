a213df51629d776cec7ae5697128c5f073eae342
Anthony Choi <froshiga@gmail.com>
2019-03-16 12:43:42 -0700

    re-original

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -1,98 +1,140 @@
 package nachos.threads;
 
 import nachos.machine.*;
-import java.util.LinkedList; 
-import java.util.Queue; 
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * A <i>communicator</i> allows threads to synchronously exchange 32-bit
- * messages. Multiple threads can be waiting to <i>speak</i>,
- * and multiple threads can be waiting to <i>listen</i>. But there should never
- * be a time when both a speaker and a listener are waiting, because the two
- * threads can be paired off at this point.
+ * messages. Multiple threads can be waiting to <i>speak</i>, and multiple
+ * threads can be waiting to <i>listen</i>. But there should never be a time
+ * when both a speaker and a listener are waiting, because the two threads can
+ * be paired off at this point.
  */
 public class Communicator {
-	  /**
-	   * Allocate a new communicator.
-	   */
-	  
-	  private int messages;
-	  private boolean if_message_in_use;
-	  private int wait_listeners;
-	  private int the_speakers;
-	  private Lock the_lock;
-	  private Condition2 speakers_Condition;
-	  private Condition2 listeners_Condition;
-	  
-	  
-	  public Communicator() {
-	    this.messages =0;
-	    this.if_message_in_use = false;
-	    this.wait_listeners =0;
-	    this.the_lock = new Lock();
-	    this.listeners_Condition = new Condition2(this.the_lock);
-	    this.speakers_Condition = new Condition2(this.the_lock);
-	  }
+	/*
+	 * Lock communicatorLock; int message; int listeners; Condition2
+	 * listeningCondition; Condition2 speakingCondition; Integer buffer; boolean
+	 * messageReady;
+	 */
+	/*
+	 * Lock communicatorLock; int message; int listeners; Condition2
+	 * listeningCondition; Condition2 speakingCondition; Integer buffer; boolean
+	 * messageReady;
+	 */
 
-	  /**
-	   * Wait for a thread to listen through this communicator, and then transfer
-	   * <i>word</i> to the listener.
-	   *
-	   * <p>
-	   * Does not return until this thread is paired up with a listening thread.
-	   * Exactly one listener should receive <i>word</i>.
-	   *
-	   * @param word the integer to transfer.
-	   */
-	  public void speak(int word) {
-	    the_lock.acquire();
-	    the_speakers++;
-	    
-	    while ((if_message_in_use)||(wait_listeners == 0)) {
-	      speakers_Condition.sleep();
-	    }
-	   
+	/**
+	 * Allocate a new communicator.
+	 */
+	/*
+	 * Semaphore mutex = new Semaphore(1); Semaphore fullslots = new Semaphore(0);
+	 * Semaphore emptyslots = new Semaphore(1); Queue<Integer> words = new
+	 * LinkedList<>();
+	 */
 
-	 
-	    if_message_in_use = true;
-	    messages = word;
-	    
-	    the_speakers--;
-	    listeners_Condition.wake();
-	    
-	    the_lock.release();
-	   
-	  }
-	    
-	    
-	  
-	  /**
-	   * Wait for a thread to speak through this communicator, and then return the
-	   * <i>word</i> that thread passed to <tt>speak()</tt>.
-	   *
-	   * @return the integer transferred.
-	   */
-	  public int listen() {
-	    
-	    the_lock.acquire();
-	    wait_listeners++;
-	    
-	    
+	Semaphore mutex = new Semaphore(1);
+	Semaphore fullslots = new Semaphore(0);
+	Semaphore emptyslots = new Semaphore(1);
+	Queue<Integer> words = new LinkedList<>();
 
-	     
-	     speakers_Condition.wake();
-	     listeners_Condition.sleep();
-	    
-	    
-	    this.thee_word = messages;
-	    if_message_in_use = false;
-	    wait_listeners--;
-	   
-	    speakers_Condition.wake();
-	    the_lock.release();
-	    
-	    return this.thee_word;
-	  }
-	  private int thee_word;
+	public Communicator() {
+		/*
+		 * communicatorLock = new Lock(); message = 0; listeners = 0; listeningCondition
+		 * = new Condition2(communicatorLock); speakingCondition = new
+		 * Condition2(communicatorLock); buffer = null; messageReady = false;
+		 * 
+		 * communicatorLock = new Lock(); message = 0; listeners = 0; listeningCondition
+		 * = new Condition2(communicatorLock); speakingCondition = new
+		 * Condition2(communicatorLock); buffer = null; messageReady = false;
+		 */
 	}
+
+	/**
+	 * @ -51,23 +48,26 @@ public class Communicator {
+	 * 
+	 * @param word the integer to transfer.
+	 */
+	public void speak(int word) {
+		/*
+		 * communicatorLock.acquire();
+		 * 
+		 * while (listeners == 0 || buffer == null) { speakingCondition.sleep(); }
+		 * 
+		 * message = word; buffer = 1;
+		 * 
+		 * listeningCondition.wake();
+		 * 
+		 * communicatorLock.release();
+		 */
+
+		/*
+		 * emptyslots.P(); mutex.P(); words.add(word); mutex.V(); fullslots.V();
+		 * communicatorLock.acquire();
+		 * 
+		 * while(listeners == 0 || buffer == null) { speakingCondition.sleep(); }
+		 * 
+		 * message = word; buffer = 1;
+		 * 
+		 * listeningCondition.wake();
+		 * 
+		 * 
+		 * communicatorLock.release();
+		 */
+
+		emptyslots.P();
+		mutex.P();
+		words.add(word);
+		mutex.V();
+		fullslots.V();
+
+	}
+
+	public int listen() {
+    	/*communicatorLock.acquire();
+    	
+    	listeners++;
+    	
+    	while(buffer == null) {
+    		listeningCondition.sleep();
+    		speakingCondition.wake();
+    	}
+    	
+    	int msg = message;
+    	buffer = null;
+    	
+    	listeners--;
+    	
+    	communicatorLock.release();
+    	
+    	return msg;*/
+		
+		/*
+		 * fullslots.P(); mutex.P(); int word = words.remove(); emptyslots.V();
+		 * communicatorLock.acquire();
+		 * 
+		 * listeners++;
+		 * 
+		 * while(buffer == null) { listeningCondition.sleep(); speakingCondition.wake();
+		 * }
+		 * 
+		 * int msg = message; buffer = null;
+		 * 
+		 * listeners--;
+		 * 
+		 * return word;
+		 * communicatorLock.release();
+		 * 
+		 * return msg;
+		 */
+		
+		
+		 fullslots.P(); 
+		 mutex.P(); 
+		 int word = words.remove(); 
+		 emptyslots.V();
+		  
+		 return word;
+		 
+		 
+    }
+}
